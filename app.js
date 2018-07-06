c6b4b272c6d8fd34a944c99cdc6ead3bcea55731
huanghanzhilian <1319639755@qq.com>
2018-07-06 17:49:02 +0800

    前台头部公共组件

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const pkg = require('./package')
 
 const app = express()
 // 设置模板目录
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, './app/views'))
 // 设置模板引擎为 ejs
 app.set('view engine', 'ejs')
 // 设置静态文件目录
