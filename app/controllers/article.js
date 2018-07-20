09f9a67cc5bc6e2f9748ebbdb8d5f353cfc339c1
huanghanzhilian <1319639755@qq.com>
2018-07-20 15:19:49 +0800

    用户端博客列表&详情页展示

--- app/controllers/article.js
+++ app/controllers/article.js
@@ -1,11 +1,41 @@
+var Article = require('../models/article.js'); // 载入mongoose编译后的模型article
+var Category = require('../models/category');
+
+const md = require('markdown-it')();
+
 // detail page 文章详情页
 exports.detail = function(req, res) {
-	console.log(req.params)
-	console.log(req.query)
-    // res.render('detail', {
-    //     title: '文章详情'
-    // });
-    res.send('文章详情页')
+	var id = req.params.id;
+	Article.findById(id, function(err, article) {
+		if (err) {
+            console.log(err);
+        }
+        article.content=md.render(article.content);
+		res.render('web/article', {
+            title: article.title,
+            content: article.content
+        });
+    });
+	// Article.findById(id, function(err, movie) {
+ //        Comment
+ //        .find({movie: id})
+ //        .populate('from', 'name')
+ //        .populate('reply.to reply.from', 'name')
+ //        .exec(function(err, comments) {
+ //            console.log(comments)
+ //            res.render('detail', {
+ //                title: 'i_movie' + movie.title,
+ //                movie: movie,
+ //                comments: comments
+ //            });
+ //        })
+ //    });
+
+
+
+	// console.log(req.params)
+	// console.log(req.query)
+    
 };
 
 
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,22 @@
+var Article = require('../models/article.js'); // 载入mongoose编译后的模型article
+var Category = require('../models/category');
+
+const md = require('markdown-it')();
+
 // index page 首页
 exports.index = function(req, res) {
-    res.render('web/index', {
-        title: '首页',
-    })
+	Article.fetch(function(err, article) {
+        if (err) {
+            console.log(err);
+        }
+        res.render('web/index', {
+            title: '首页',
+            articles: article
+        });
+    });
+    // res.render('web/index', {
+    //     title: '首页',
+    // })
     //res.send('首页')
 }
 
