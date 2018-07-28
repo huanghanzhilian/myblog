e1ccb25b1f05bf57853bdbb05fdc621ea0666d8e
huanghanzhilian <1319639755@qq.com>
2018-07-28 22:06:07 +0800

    多表联查

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,15 +5,37 @@ const md = require('markdown-it')();
 
 // index page 首页
 exports.index = function(req, res) {
-	Article.fetch(function(err, article) {
-        if (err) {
-            console.log(err);
-        }
-        res.render('web/index', {
-            title: '首页',
-            articles: article
-        });
-    });
+    Article.find()
+        .populate('categoryid', 'name')
+        .exec(function(err, articles) {
+            Category.fetch(function(err, categorys) {
+                if (err) {
+                    console.log(err);
+                }
+                // res.render('admin/categorymanage', {
+                //     title: '文章分类管理',
+                //     categorys: categorys
+                // });
+                console.log(categorys)
+                res.render('web/index', {
+                    title: '首页',
+                    articles: articles,
+                    categorys:categorys
+                });
+            });
+            
+        })
+
+
+	// Article.fetch(function(err, article) {
+ //        if (err) {
+ //            console.log(err);
+ //        }
+ //        res.render('web/index', {
+ //            title: '首页',
+ //            articles: article
+ //        });
+ //    });
     // res.render('web/index', {
     //     title: '首页',
     // })
